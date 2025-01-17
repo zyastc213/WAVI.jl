65bba9eb0cd1682060461a6ac9ab8780648e7dfd
James Byrne <jamescircadian@gmail.com>
2025-01-17 17:38:22 +0000

    Also ensure that checkpoints go into the output directory

--- src/Simulations/run_simulation.jl
+++ src/Simulations/run_simulation.jl
@@ -22,23 +22,23 @@ update_thickness!(model::AbstractModel)
 Update thickness using rate of change of thickness and apply minimum thickness constraint. Includes an option for not evolving shelf thickness.
 """
 function update_thickness!(simulation)
-@unpack model,timestepping_params=simulation
-hUpdate=zeros(model.grid.nx,model.grid.ny)
-aground=zeros(model.grid.nx,model.grid.ny)
-hUpdate[model.fields.gh.mask]=max.(model.params.minimum_thickness .- model.fields.gh.h[model.fields.gh.mask],timestepping_params.dt*model.fields.gh.dhdt[model.fields.gh.mask])
-#Specify whether to evolve the shelves:
-if !model.params.evolveShelves
-    hUpdate[model.fields.gh.mask]=max.(model.params.smallHAF.-(model.params.density_ocean./model.params.density_ice).*model.fields.gh.b[model.fields.gh.mask].-model.fields.gh.h[model.fields.gh.mask],hUpdate[model.fields.gh.mask])
-    aground=(model.fields.gh.haf.>=0)
-    wc=[1 1 1; 1 1 1; 1 1 1]
-    w=centered(wc)
-    nearfloat_mask=imfilter(model.fields.gh.mask.&.!aground,reflect(w),Fill(0,w))
-    nearfloat_mask=iszero.(iszero.(nearfloat_mask))
-    hUpdate[nearfloat_mask].=0
-end
-hUpdate[model.fields.gh.h_isfixed] .= 0
-model.fields.gh.h[model.fields.gh.mask] = model.fields.gh.h[model.fields.gh.mask] .+ hUpdate[model.fields.gh.mask]
-return simulation
+    @unpack model,timestepping_params=simulation
+    hUpdate=zeros(model.grid.nx,model.grid.ny)
+    aground=zeros(model.grid.nx,model.grid.ny)
+    hUpdate[model.fields.gh.mask]=max.(model.params.minimum_thickness .- model.fields.gh.h[model.fields.gh.mask],timestepping_params.dt*model.fields.gh.dhdt[model.fields.gh.mask])
+    #Specify whether to evolve the shelves:
+    if !model.params.evolveShelves
+        hUpdate[model.fields.gh.mask]=max.(model.params.smallHAF.-(model.params.density_ocean./model.params.density_ice).*model.fields.gh.b[model.fields.gh.mask].-model.fields.gh.h[model.fields.gh.mask],hUpdate[model.fields.gh.mask])
+        aground=(model.fields.gh.haf.>=0)
+        wc=[1 1 1; 1 1 1; 1 1 1]
+        w=centered(wc)
+        nearfloat_mask=imfilter(model.fields.gh.mask.&.!aground,reflect(w),Fill(0,w))
+        nearfloat_mask=iszero.(iszero.(nearfloat_mask))
+        hUpdate[nearfloat_mask].=0
+    end
+    hUpdate[model.fields.gh.h_isfixed] .= 0
+    model.fields.gh.h[model.fields.gh.mask] = model.fields.gh.h[model.fields.gh.mask] .+ hUpdate[model.fields.gh.mask]
+    return simulation
 end
 
 """
@@ -67,7 +67,7 @@ function run_simulation!(simulation)
         #check if we have hit a temporary checkpoint
         if mod(i,timestepping_params.n_iter_chkpt) == 0
             #output a temporary checkpoint
-            fname = string("Chkpt",chkpt_tag, ".jld2")
+            fname = joinpath(output_params.output_path, string("Chkpt",chkpt_tag, ".jld2"))
             @save fname simulation
             chkpt_tag = (chkpt_tag == "A") ? "B" : "A"
             println("making temporary checkpoint at timestep number $(simulation.clock.n_iter)")
@@ -77,7 +77,7 @@ function run_simulation!(simulation)
         if mod(i,simulation.timestepping_params.n_iter_pchkpt) == 0
             #output a permanent checkpoint
             n_iter_string =  lpad(simulation.clock.n_iter, 10, "0"); #filename as a string with 10 digits
-            fname = string("PChkpt_",n_iter_string, ".jld2")
+            fname = joinpath(output_params.output_path, string("PChkpt_",n_iter_string, ".jld2"))
             @save fname simulation
             println("making permanent checkpoint at timestep number $(simulation.clock.n_iter)")
         end
